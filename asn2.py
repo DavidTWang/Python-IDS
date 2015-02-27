d62a0b43efc9cb8c93190fd81356ac608d7f9de1
Hakus <archeid@gmail.com>
2015-02-27 12:33:40 -0800

    Added timeout and Linux disto check, amongst other things

--- asn2.py
+++ asn2.py
@@ -1,10 +1,21 @@
-import pyinotify, re, os
+import pyinotify, re, os, ConfigParser, threading, sys, platform
 
-watch_file = "/var/log/secure"
-
-wm = pyinotify.WatchManager()
-file_events = pyinotify.IN_MODIFY
 connections = {}
+timeout = 0
+
+def ban_ip(ip, service=""):
+	os.system("iptables -A INPUT -p tcp --dport ssh -s %s -j DROP" % ip)
+	if(timeout != 0):
+		unban = Timer(timeout, unban_ip, args=[ip,])
+
+def unban_ip(ip, service=""):
+	os.system("iptables -D INPUT -p tcp --dport ssh -s %s -j DROP" % ip)
+
+def reset_iptables():
+	os.system("iptables -F")
+
+def reset_cfg():
+	pass
 
 def get_last_lines(file):
 	with open(file, "r") as f:
@@ -18,33 +29,44 @@ def get_last_lines(file):
 			return line
 			break
 
-def ban_ip(ip):
-	os.system("iptables -A INPUT -s %s -j DROP" % ip)
-
 class EventHandler(pyinotify.ProcessEvent):
 	def process_IN_MODIFY(self, event):
 		line = get_last_lines(event.pathname)
 		if(line is not None):
-			ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
-			print "Bad SSH login from %s" % ip[0]
+			ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)[0]
+			print "Bad SSH login from %s" % ip
 			try:
-				if(connections[ip[0]] is None):
+				if(connections[ip] is None):
 					pass
 				else:
-					connections[ip[0]] += 1
-					if(connections[ip[0]] == 2):
-						ban_ip(ip[0])
+					connections[ip] += 1
+					if(connections[ip] == 2):
+						ban_ip(ip)
 			except KeyError:
-				connections[ip[0]] = 1
+				connections[ip] = 1
 
+def main():
+	if(platform.dist()[0] == "Ubuntu"):
+		watch_file = "/var/log/auth.log"
+	else:
+		watch_file = "/var/log/secure"
+	wm = pyinotify.WatchManager()
+	file_events = pyinotify.IN_MODIFY
 
+	handler = EventHandler()
+	notifier = pyinotify.Notifier(wm, handler)
+	wdd = wm.add_watch(watch_file, file_events, rec=True)
 
-handler = EventHandler()
-notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch(watch_file, file_events, rec=True)
+	print "running..."
+	notifier.loop()
 
-print "running..."
-notifier.loop()
+if __name__ == '__main__':
 
-# if __name__ == '__main__':
-	# main()
+	if(len(sys.argv) == 3):
+		timeout = int(sys.argv[2])
+		main()
+	elif(len(sys.argv) == 1):
+		main()
+	else:
+		print "Format: python pyIDS.py [-t timeout]"
+		print "-t : Time till IPs get unbanned in seconds, default value of 0 never unbans"
